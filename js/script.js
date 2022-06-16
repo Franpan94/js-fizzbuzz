3cfe346a432280d8c1f2420e60a9530487b59a1e
Franpan94 <francescopanico94@libero.it>
2022-06-16 16:22:33 +0200

    I cannot associate the color to the various boxes

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
+//Dichiaro una variabile 
 let counter= document.getElementById('counter-element');
 
-for(i=1; i<=100; i++){
+// Apro il ciclo for dichiarando un contatore 
+for(i=2; i<=100; i++){
     
+    // Creo un div 
     let box= document.createElement('div');
-    box.classList.add('col', 'ms_box');
+
+    // Aggiungo delle classi al div creato
+    box.classList.add('col-3', 'ms_box',);
     
+    
+    // Appendo alla variabile dichiarata all'esterno del ciclo il div-box creato
     counter.append(box);
     
-
+    
+   //Dichiaro la condizione che nel caso in cui sia divisibile per 3 e per 5 con resto=0 stampi la voce fizzbuzz 
    if(((i % 3)==0) && ((i % 5)==0)){
         box.innerHTML= 'FizzBuzz';
         console.log('FizzBuzz');
+    
+     //Dichiaro la condizione che nel caso in cui sia divisibile per 3 con resto=0 stampi la voce fizz 
     } else if((i % 3)==0){
         box.innerHTML= 'Fizz';
         console.log('Fizz');
     
+     //Dichiaro la condizione che nel caso in cui sia divisibile per 5 con resto=0 stampi la voce buzz
     } else if((i % 5)==0){
         box.innerHTML= 'Buzz';
         console.log('Buzz');  
     
+    // Altrimenti stampa semplicemente i numeri
     } else{
         box.innerHTML=i;
         console.log(i);
